163b41b8648e9a2ceac028e5b3714966bfdd500b
Mariam <mariam.moustafa1997@gmail.com>
2018-11-23 15:43:50 +0200

    A

--- src/app/recipes/recipe-deatil/recipe-deatil.component.ts
+++ src/app/recipes/recipe-deatil/recipe-deatil.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-deatil',
@@ -8,11 +10,37 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-deatil.component.css']
 })
 export class RecipeDeatilComponent implements OnInit {
-  @Input() recipe: Recipe;
+  recipe: Recipe;
+  id: number;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService,
+              private route: ActivatedRoute,
+              private router: Router) {
+  }
 
   ngOnInit() {
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.id = +params['id'];
+          this.recipe = this.recipeService.getRecipe(this.id);
+        }
+      );
+  }
+
+  onAddToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], {relativeTo: this.route});
+    
+  }
+
+  onDeleteRecipe() {
+    this.recipeService.deleteRecipe(this.id);
+    this.router.navigate(['/recipes']);
   }
 
 }
+
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,14 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Recipe } from './recipe.model';
-
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
-  selectedRecipe: Recipe;
 
   constructor() { }
 
@@ -16,3 +13,4 @@ export class RecipesComponent implements OnInit {
   }
 
 }
+
